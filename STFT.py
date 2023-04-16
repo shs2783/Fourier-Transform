b9f6248a6a4a0a225f1e6c3e40366f55e59d9856
shs2783 <shs2783_@naver.com>
2023-04-16 23:59:13 +0900

    update STFT and test with torch stft

--- STFT.py
+++ STFT.py
@@ -2,97 +2,79 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-def stft(x, window_size, hop_size):
-    window = np.hanning(window_size)
-    num_frames = int(np.ceil(len(x) / hop_size))
-    stft_matrix = np.zeros((window_size, num_frames), dtype=np.complex128)
+def stft(signal, fft_size=1024, window_size=1024, hop_size=256):
+    num_samples = len(signal)
+    num_pad_samples = len(signal) + fft_size
 
-    for i in range(num_frames):
-        start = i * hop_size
-        end = min(start + window_size, len(x))
+    # Padding the signal so that frames are centered around their midpoint
+    signal = np.pad(signal, int(fft_size // 2), mode='reflect')
+    window = np.hanning(window_size)  # = signal.windows.hann(window_size, sym=False)
 
-        frame = x[start:end] * window[:end-start]
-        stft_matrix[:, i] = np.fft.fft(frame, window_size)
+    ### method 1 (for loop)
+    # num_freq = window_size//2 + 1
+    # num_frames = math.ceil(num_samples / hop_size)
+    # stft_matrix = np.zeros((num_frames, num_freq), dtype=np.complex128)
+
+    # for i in range(num_frames):
+    #     start = i * hop_size
+    #     end = start + fft_size
+
+    #     frame = window * signal[start:end]
+    #     stft_matrix[i] = np.fft.rfft(frame)
+
+    ### method 2 (list comprehension)
+    x_w = np.array([window * signal[i:i + fft_size] for i in range(0, num_samples, hop_size)])
+    stft_matrix = np.fft.rfft(x_w, axis=-1)
 
     return stft_matrix
 
-def istft(stft_matrix, window_size, hop_size):
-    num_samples = (stft_matrix.shape[1] - 1) * hop_size + window_size
+def istft(stft_matrix, window_size=1024, fft_size=1024, hop_size=256):
+    num_frames, num_freq = stft_matrix.shape
+    num_samples = (num_frames-1) * hop_size + fft_size
+    window = np.hanning(window_size)  # = signal.windows.hann(window_size, sym=False)
 
-    window = np.hanning(window_size)
-    output = np.zeros(num_samples)
+    signal = np.zeros(num_samples)
     window_sum = np.zeros(num_samples)
 
-    for i in range(stft_matrix.shape[1]):
+    x_w = np.fft.irfft(stft_matrix, axis=-1)
+    for i in range(num_frames):
         start = i * hop_size
-        end = start + window_size
+        end = start + fft_size
 
-        frame = np.real(np.fft.ifft(stft_matrix[:, i], window_size))
-
-        output[start:end] += frame * window
+        signal[start:end] += window * x_w[i]
         window_sum[start:end] += window
 
     # Normalize the output signal by dividing it by the sum of the window values
-    output /= window_sum
-
-    return output
-
-
-def stft(x, fft_size=1024, hop_size=256):
-    print('input signal', x.shape)
-    w = np.hanning(fft_size)
-    # w = signal.windows.hann(fft_size, sym=False)
-
-    # Padding the signal so that frames are centered around their midpoint
-    x = np.pad(x, int(fft_size // 2), mode='reflect')
-    print('padding signal', x.shape)
-
-    # Window the signal
-    x_w = np.array([w * x[i:i + fft_size] for i in range(0, len(x) - fft_size, hop_size)])
-    print('window signal', x_w.shape) # = math.ceil((len(x) - fft_size) / hop_size)
-
-    # Compute the FFT
-    X = np.fft.rfft(x_w, axis=-1)
-    print('rfft', X.shape)
-
-    return X
-
-def istft(X, fft_size=1024, hop_size=256):
-    w = np.hanning(fft_size)
-    # w = signal.windows.hann(fft_size, sym=False)
-
-    # Compute the IFFT
-    x_w = np.fft.irfft(X, axis=-1)
-    print('irfft', x_w.shape)
-
-    # Overlap-add the windows
-    x = np.zeros((len(x_w)-1)*hop_size + fft_size)
-    for n,i in enumerate(range(0, len(x) - fft_size, hop_size)):
-        x[i:i+fft_size] += w * x_w[n]
-    print('overlap add', x.shape)
+    signal /= window_sum
 
     # Remove padding
-    x = x[int(fft_size // 2):-int(fft_size // 2)]
-    print('remove padding output signal', x.shape)
+    signal = signal[int(fft_size // 2): -int(fft_size // 2)]
 
-    return x
+    return signal
 
-second = 10
+
+# Generate a test signal
+second = 1
 sampling_rate = 16000
 fs = sampling_rate * second  # Total number of samples
 t = np.linspace(0, second, fs)  # Time vector
 
 x = 0
-for Hz in range(1, 10, 2):
-    x += 1/Hz * np.sin( 2*np.pi * Hz*t )  # Input signal
+x += np.sin( 2*np.pi * 440*t )  # Input signal
+x += np.sin( 2*np.pi * 1320*t )  # Input signal
+
+
+# Set the parameters
+window_size = 1024
+fft_size = 1024
+hop_size = 256
 
 # Compute the STFT of the test signal
-window_size = 1024
-hop_size = 256
-stft_matrix = stft(x, window_size, hop_size)
+stft_matrix = stft(x, window_size, fft_size, hop_size)
 
 # Compute the inverse STFT of the STFT matrix
-x_hat = istft(stft_matrix, window_size, hop_size)
+x_hat = istft(stft_matrix, window_size, fft_size, hop_size)
+
 
 # Plot the magnitude of the STFT
 plt.imshow(np.abs(stft_matrix).T, origin='lower', aspect='auto', cmap='inferno')
@@ -101,8 +83,9 @@ plt.ylabel('Frequency (bins)')
 plt.show()
 
 # Plot the original and reconstructed signals
-t2 = np.linspace(0, 1, len(x_hat))
 fig, axs = plt.subplots(2, 1)
+t = np.linspace(0, second, len(x))
+t2 = np.linspace(0, second, len(x_hat))
 
 axs[0].plot(t, x, label='Original signal')
 axs[1].plot(t2, x_hat, label='Reconstructed signal')
@@ -112,7 +95,18 @@ plt.legend()
 plt.show()
 
 
+# Test with torchaudio
 import torch
 import torchaudio
-torchaudio.save('test.wav', torch.tensor(x).float().unsqueeze(0), 44100)
-torchaudio.save('test_hat.wav', torch.tensor(x_hat).float().unsqueeze(0), 44100)
+
+torch_stft_matrix = torch.stft(torch.tensor(x).float(), fft_size, hop_size, window_size, window=torch.hann_window(window_size), center=True, normalized=False, onesided=True, pad_mode='reflect')
+torch_stft_matrix = torch_stft_matrix[..., 0] + 1j * torch_stft_matrix[..., 1]
+torch_stft_matrix = torch_stft_matrix.numpy().T
+
+stft_matrix = stft(x, window_size, fft_size, hop_size)
+x_hat = istft(stft_matrix, window_size, fft_size, hop_size)
+x_hat_torch = istft(torch_stft_matrix, window_size, fft_size, hop_size)
+
+torchaudio.save('test.wav', torch.tensor(x).float().unsqueeze(0), sampling_rate)
+torchaudio.save('test_hat.wav', torch.tensor(x_hat).float().unsqueeze(0), sampling_rate)
+torchaudio.save('test_hat_torch.wav', torch.tensor(x_hat_torch).float().unsqueeze(0), sampling_rate)
